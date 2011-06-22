851cf03ec836c3a933d54af79ec4af21db4555e4
t_f_m <ohmizaiju@s33.xrea.com>
2011-06-23 03:04:30 +0900

    XPath式を一種類にまとめて管理を楽に

--- a_smart_dashboard_is_reb.user.js
+++ a_smart_dashboard_is_reb.user.js
@@ -8,16 +8,16 @@
 // ==/UserScript==
 (function() {
 var css = <![CDATA[
-li[id^="post"][class*="is_reblogged_mine"] {
+li[id^="post"].is_reblogged_mine {
 		-moz-opacity:0.9 !important;
 		opacity:0.9 !important;
 		max-height:1em !important;
 		overflow:hidden !important;
 	}
-li[id^="post"][class*="is_reblogged_mine"] img {
+li[id^="post"].is_reblogged_mine img {
 		display:none !important;
 	}
-li[id^="post"][class*="is_reblogged_mine"] blockquote {
+li[id^="post"].is_reblogged_mine blockquote {
 		display:none !important;
 }
 ]]>.toString();
@@ -38,26 +38,27 @@ if (typeof GM_addStyle != "undefined") {
 	}
 }
 
-function is_reblogged_mine(doc, xpath){
-  var target = document.evaluate(xpath,doc,null,7,null);
+var XPATH='./descendant-or-self::li[contains(@class,"not_mine")][contains(@class,"is_reblog")][div[@class="post_info"][contains(.,"reblogged you") or contains(.,"があなたからリブログ")]]';
+
+function is_reblogged_mine(doc){
+  var target = document.evaluate(XPATH,doc,null,7,null);
   for(var i = 0, maxi = target.snapshotLength; i < maxi; i++){
-    target.snapshotItem(i).className += " is_reblogged_mine ";//" notification is_reblogged_mine ";
+    target.snapshotItem(i).className += " is_reblogged_mine ";
   }
 }
 
-var hookKeyWord="reblogged you";
+//var hookKeyWord="reblogged you";
 
 var boot=function(){
-  is_reblogged_mine(document,'.//li[contains(@class,"not_mine")][contains(@class,"is_reblog")][div[@class="post_info"][contains(.,"reblogged you") or contains(.,"があなたからリブログ")]]');
   window.addEventListener('AutoPagerize_DOMNodeInserted', function(evt){
-    is_reblogged_mine(evt.target,'.//div[@class="post_info"][contains(.,"reblogged you") or contains(.,"があなたからリブログ")]/parent::li');
+    is_reblogged_mine(evt.target);
   }, false);
 }
+
+is_reblogged_mine(document);
 if(window.AutoPagerize){
   boot();
 }else{
   window.addEventListener('GM_AutoPagerizeLoaded', boot, false);
 }
 })();
-
-//  ./descendant-or-self::li[contains(@class,"not_mine")][contains(@class,"is_reblog")][div[@class="post_info"][contains(.,"reblogged you") or contains(.,"があなたからリブログ")]]
