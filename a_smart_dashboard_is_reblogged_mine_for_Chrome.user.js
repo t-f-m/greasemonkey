ee71b0a7c422bf86034ff83d282f3647c7e20bec
t_f_m <ohmizaiju@s33.xrea.com>
2011-06-23 03:14:09 +0900

    メインのスクリプトに合わせて変更

--- a_smart_dashboard_is_reblogged_mine_for_Chrome.user.js
+++ a_smart_dashboard_is_reblogged_mine_for_Chrome.user.js
@@ -8,20 +8,20 @@
 // ==/UserScript==
 (function() {
 /*var css = (<![CDATA[
-li[id^="post"][class*="is_reblogged_mine"] {
+li[id^="post"].is_reblogged_mine {
 		-moz-opacity:0.9 !important;
 		opacity:0.9 !important;
 		max-height:1em !important;
 		overflow:hidden !important;
 	}
-li[id^="post"][class*="is_reblogged_mine"] img {
+li[id^="post"].is_reblogged_mine img {
 		display:none !important;
 	}
-li[id^="post"][class*="is_reblogged_mine"] blockquote {
+li[id^="post"].is_reblogged_mine blockquote {
 		display:none !important;
 }
 ]]>).toString();*/
-var css="li[id^=\"post\"][class*=\"is_reblogged_mine\"] {\n		-moz-opacity:0.9 !important;\n		opacity:0.9 !important;\n		max-height:1em !important;\n		overflow:hidden !important;\n	}\n	li[id^=\"post\"][class*=\"is_reblogged_mine\"] img {\n		display:none !important;\n	}\n	li[id^=\"post\"][class*=\"is_reblogged_mine\"] blockquote {\n		display:none !important;\n	}";
+var css="li[id^=\"post\"].is_reblogged_mine {\n		-moz-opacity:0.9 !important;\n		opacity:0.9 !important;\n		max-height:1em !important;\n		overflow:hidden !important;\n	}\n	li[id^=\"post\"].is_reblogged_mine img {\n		display:none !important;\n	}\n	li[id^=\"post\"].is_reblogged_mine blockquote {\n		display:none !important;\n	}";
 
 if (typeof GM_addStyle != "undefined") {
 	GM_addStyle(css);
@@ -39,28 +39,28 @@ if (typeof GM_addStyle != "undefined") {
 	}
 }
 
-function is_reblogged_mine(doc, xpath){
-  var target = document.evaluate(xpath,doc,null,7,null);
+var XPATH='./descendant-or-self::li[contains(@class,"not_mine")][contains(@class,"is_reblog")][div[@class="post_info"][contains(.,"reblogged you") or contains(.,"があなたからリブログ")]]';
+
+function is_reblogged_mine(doc){
+  var target = document.evaluate(XPATH,doc,null,7,null);
   for(var i = 0, maxi = target.snapshotLength; i < maxi; i++){
-    target.snapshotItem(i).className += " is_reblogged_mine ";//" notification is_reblogged_mine ";
+    target.snapshotItem(i).className += " is_reblogged_mine ";
   }
 }
 
 var boot=function(){
-  is_reblogged_mine(document,'.//li[contains(@class,"not_mine")][contains(@class,"is_reblog")][div[@class="post_info"][text()[contains(self::text(),"reblogged you") or contains(self::text(),"があなたからリブログ")]]]');
   window.addEventListener('AutoPagerize_DOMNodeInserted', function(evt){
-    is_reblogged_mine(evt.target,'.//div[@class="post_info"][text()[contains(self::text(),"reblogged you") or contains(self::text(),"があなたからリブログ")]]/parent::li');
+    is_reblogged_mine(evt.target);
   }, false);
   window.addEventListener('AutoPatchWork.DOMNodeInserted', function(evt){
-    is_reblogged_mine(evt.target,'.//div[@class="post_info"][text()[contains(self::text(),"reblogged you") or contains(self::text(),"があなたからリブログ")]]/parent::li');
+    is_reblogged_mine(evt.target);
   }, false);
 }
 //if(window.AutoPagerize){
-  boot();
+is_reblogged_mine(document);
+boot();
 //}else{
 //  window.addEventListener('GM_AutoPagerizeLoaded', boot, false);
 //  window.addEventListener('AutoPatchWork.initialized',boot,false);
 //}
 })();
-
-//  ./descendant-or-self::li[contains(@class,"not_mine")][contains(@class,"is_reblog")][div[@class="post_info"][text()[contains(self::text(),"reblogged you")]]]
